29683dcd0f8b3108a09c05c8c0a182a0481836d5
mrrevo55 <kwolny31@gmail.com>
2022-09-23 12:41:23 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/statsreadservice/StatsReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.statsreadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsReadServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcivilization/statsreadservice/StatsReadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.statsreadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsReadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
